cab1b88e3061ac4b8d85fd857e19b3cf34ba3d99
TastySponge~ <ktpham.apr@gmail.com>
2026-08-20 14:47:20 +0700

    Sprint 2: Tạo cấu trúc thư mục, Path Alias, gọi Fetch API hiển thị HomeScreen

--- App.tsx
+++ App.tsx
@@ -7,6 +7,20 @@ import StateDemo from './src/screens/demos/StateDemo';
 import DemoPropsState from './src/screens/demos/DemoPropsState';
 import StyleSheetWalkthrough from './src/screens/demos/StyleSheetWalkthrough';
 import FetchDemo from './src/screens/demos/FetchDemo';
+import ActivityIndicator from './src/screens/demos/ActivityIndicator';
+import Modal from './src/screens/demos/Modal';
+import SwitchDemo from './src/screens/demos/Switch';
+import Alert from './src/screens/demos/Alert';
+import SafeAreaView from './src/screens/demos/SafeAreaView';
+import SectionListDemo from './src/screens/demos/SectionList';
+import FlatListDemo from './src/screens/demos/FlatList';
+import TouchableOpacityDemo from './src/screens/demos/TouchableOpacity';
+import ScrollViewDemo from './src/screens/demos/ScrollView';
+import ViewDemo from './src/screens/demos/ViewDemo';
+import TextViewDemo from './src/screens/demos/TextView';
+import ImageView from './src/screens/demos/ImageView';
+import TextInput from './src/screens/demos/TextInput';
+import Skeleton from './src/screens/demos/Skeleton';
 
 function App(): React.JSX.Element {
   return (
@@ -17,8 +31,23 @@ function App(): React.JSX.Element {
       {/* <StateDemo /> */}
       {/* <DemoPropsState /> */}
       {/* <StyleSheetWalkthrough /> */}
-      <FetchDemo />
-      {/* <StyleSheetWalkthrough /> */}
+      {/* <FetchDemo /> */}
+      {/* <ActivityIndicator /> */}
+      {/* <Modal /> */}
+      {/* <SwitchDemo /> */}
+      {/* <Alert /> */}
+      {/* <SafeAreaView /> */}
+      {/* <SectionListDemo /> */}
+      {/* <FlatListDemo /> */}
+      {/* <TouchableOpacityDemo /> */}
+      {/* <ScrollViewDemo /> */}
+      {/* <ViewDemo /> */}
+      {/* <TextViewDemo /> */}
+      {/* <ImageView /> */}
+      {/* <TextInput /> */}
+      <Skeleton />
+
+
     </SafeAreaProvider>
   );
 }
