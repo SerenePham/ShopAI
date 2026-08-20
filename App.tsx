28efe8211936f67b9e33b39b7a05d049a11e2677
TastySponge~ <ktpham.apr@gmail.com>
2026-08-20 14:13:19 +0700

    Sprint 2: Tạo cấu trúc thư mục, Path Alias, gọi Fetch API hiển thị HomeScreen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import HomeScreen from '@screens/HomeScreen';
+import JsxWelcomeDemo from './src/screens/demos/JsxWelcomeDemo';
+import PropsDemo from './src/screens/demos/PropsDemo';
+import StateDemo from './src/screens/demos/StateDemo';
+import DemoPropsState from './src/screens/demos/DemoPropsState';
+import StyleSheetWalkthrough from './src/screens/demos/StyleSheetWalkthrough';
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaProvider>
-      <HomeScreen />
+      {/* <HomeScreen /> */}
+      {/* <JsxWelcomeDemo /> */}
+      {/* <PropsDemo /> */}
+      {/* <StateDemo /> */}
+      <DemoPropsState />
+      {/* <StyleSheetWalkthrough /> */}
     </SafeAreaProvider>
   );
 }
