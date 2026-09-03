db69b873de49cbf9be4c891c1d5d8ab38b6eaf55
TastySponge~ <ktpham.apr@gmail.com>
2026-09-03 13:38:05 +0700

    Sprint 3: Design System — Typography, ShopInput, ShopButton, useCountdown, ThemeContext Dark Mode

--- App.tsx
+++ App.tsx
@@ -1,56 +1,13 @@
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { ThemeProvider } from "@contexts/ThemeContext";
+import HomeScreen from "@screens/HomeScreen";
 
-import HomeScreen from '@screens/HomeScreen';
-import JsxWelcomeDemo from './src/screens/demos/JsxWelcomeDemo';
-import PropsDemo from './src/screens/demos/PropsDemo';
-import StateDemo from './src/screens/demos/StateDemo';
-import DemoPropsState from './src/screens/demos/DemoPropsState';
-import StyleSheetWalkthrough from './src/screens/demos/StyleSheetWalkthrough';
-import FetchDemo from './src/screens/demos/FetchDemo';
-import ActivityIndicator from './src/screens/demos/ActivityIndicator';
-import Modal from './src/screens/demos/Modal';
-import SwitchDemo from './src/screens/demos/Switch';
-import Alert from './src/screens/demos/Alert';
-import SafeAreaView from './src/screens/demos/SafeAreaView';
-import SectionListDemo from './src/screens/demos/SectionList';
-import FlatListDemo from './src/screens/demos/FlatList';
-import TouchableOpacityDemo from './src/screens/demos/TouchableOpacity';
-import ScrollViewDemo from './src/screens/demos/ScrollView';
-import ViewDemo from './src/screens/demos/ViewDemo';
-import TextViewDemo from './src/screens/demos/TextView';
-import ImageView from './src/screens/demos/ImageView';
-import TextInput from './src/screens/demos/TextInput';
-import Skeleton from './src/screens/demos/Skeleton';
-import RootNavigator from '@navigation/RootNavigator';
-
-function App(): React.JSX.Element {
+function App() {
   return (
     <SafeAreaProvider>
-      <HomeScreen />
-      {/* <JsxWelcomeDemo /> */}
-      {/* <PropsDemo /> */}
-      {/* <StateDemo /> */}
-      {/* <DemoPropsState /> */}
-      {/* <StyleSheetWalkthrough /> */}
-      {/* <FetchDemo /> */}
-      {/* <ActivityIndicator /> */}
-      {/* <Modal /> */}
-      {/* <SwitchDemo /> */}
-      {/* <Alert /> */}
-      {/* <SafeAreaView /> */}
-      {/* <SectionListDemo /> */}
-      {/* <FlatListDemo /> */}
-      {/* <TouchableOpacityDemo /> */}
-      {/* <ScrollViewDemo /> */}
-      {/* <ViewDemo /> */}
-      {/* <TextViewDemo /> */}
-      {/* <ImageView /> */}
-      {/* <TextInput /> */}
-      {/* <Skeleton /> */}
-      {/* <RootNavigator /> */}
-
-
+      <ThemeProvider>
+        <HomeScreen />
+      </ThemeProvider>
     </SafeAreaProvider>
   );
 }
